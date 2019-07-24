617ee2db54c90123edf3cf685be77c086c672166
tamabuchi <cr7.soccer.5324@gmail.com>
2019-07-24 11:57:33 +0800

    create form and check page

---
+++ check.php
@@ -0,0 +1,20 @@
+<?php
+    $nickname = $_POST['nickname'];
+    echo $nickname;
+
+    $email = $_POST['email']
+    echo $email;
+
+    $contact = $_POST['contact']
+    echo $contact;
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>入力内容確認</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>入力内容確認</h1>
+</body>
+</html>
