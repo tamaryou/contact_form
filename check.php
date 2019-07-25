2750fb09caf5f036c4ffd44efb3d76ad3b8b050c
tamabuchi <cr7.soccer.5324@gmail.com>
2019-07-25 10:38:58 +0800

    メッセージ

--- check.php
+++ check.php
@@ -1,12 +1,29 @@
 <?php
     $nickname = $_POST['nickname'];
-    echo $nickname;
+    
+    $email = $_POST['email'];
+    
 
-    $email = $_POST['email']
-    echo $email;
+    $content = $_POST['content'];
+    
+    if ($nickname == '') {
+        $nickname_result = 'ニックネームが入力されていません。';
+    } else {
+        $nickname_result = 'ようこそ、' . $nickname .'様';
+    }
+    
+    if ($email == '') {
+        $email_result = 'メールアドレスが入力されていません。';
+    } else {
+        $email_result = 'メールアドレス：' . $email;
+    }
 
-    $contact = $_POST['contact']
-    echo $contact;
+    if ($content == '') {
+        $content_result =  'お問い合わせ内容が入力されていません。';
+    } else {
+        $content_result = 'お問い合わせ内容：' . $content;
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -15,6 +32,19 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <h1>入力内容確認</h1>
+<h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+
+    <form method="POST" action="thanks.php">
+    <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+</form>
 </body>
 </html>
---
+++ thanks.php
@@ -0,0 +1,18 @@
+<?php
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>送信完了</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>お問い合わせありがとうございました！</h1>
+    <p><?php echo $nickname; ?></p>
+    <p><?php echo $email; ?></p>
+    <p><?php echo $content; ?></p>
+</body>
+</html>
